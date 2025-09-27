09b5d74777f37bb57ee5fd3fc40ae5f351f061ac
Checkm8cat <evandakota2010@gmail.com>
2025-09-26 20:46:31 -0700

    added dash thingy may come with alot of bugs

--- Scripts.gd
+++ Scripts.gd
@@ -5,25 +5,40 @@ const JUMP_VELOCITY = -2000.0
 var slot1 = 0
 var slot2 = 0
 var dash_cooldown = 0
+var c_velocityx = 0
+var c_velocityy = 0
+
 
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if is_on_floor():
+		$Timer.stop()
 		dash_cooldown = 0
 	if not is_on_floor():
 		velocity += 2 * get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor() and(slot1 == 2 or slot2 == 2):
+
 		velocity.y = JUMP_VELOCITY
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 
+	if velocity.y >= 2000:
+		velocity.y = 2000
+
+
+
+
 	if Input.is_action_just_pressed("Dash") and(slot1 == 1 or slot2 == 1) and dash_cooldown == 0:
+		$Timer.start()
+		c_velocityx = velocity.x
+		c_velocityy = velocity.y
+		velocity.y = 0
 		velocity.x = direction * SPEED * 10
 		dash_cooldown = 1
 		pass
@@ -45,4 +60,13 @@ func _on_circut_grabbed(variant) -> void:
 	else: if(slot2 == 0):
 		slot2 = variant
 	
+	pass # Replace with function body.
+	
+	
+
+
+func _on_timer_timeout() -> void:
+	velocity.y = c_velocityy
+	c_velocityx = 0
+	c_velocityy = 0
 	pass # Replace with function body.
--- button.gd
+++ button.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
-signal button_activated(Link_Tag)
-signal button_deactivated(Link_Tag)
+signal button_activated(Link_tag)
+signal button_deactivated(Link_tag)
 
 
 
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	button_activated.emit(get_meta("Link_Tag"))
-	print("AHHHH")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	button_activated.emit(get_meta("Link_tag"))
+	print("eeere")
 	pass # Replace with function body.
 
 
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print("eeerrr")
 	pass # Replace with function body.
